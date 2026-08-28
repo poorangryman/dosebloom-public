4f729bccba12527a01d1a4e179df9414a1821c9e
poorangryman <k-tarasov73@yandex.ru>
2026-08-28 13:54:00 +0400

    Prepare public repository for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.dosebloom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.4.3"
+        versionCode = 12
+        versionName = "1.4.4"
     }
 
     signingConfigs {
