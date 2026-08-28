ae92d96c1c05c08e615735121226081ab66a479e
poorangryman <k-tarasov73@yandex.ru>
2026-08-28 13:38:00 +0400

    Restore public release version 1.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.dosebloom.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.4.2"
+        versionCode = 11
+        versionName = "1.4.3"
     }
 
     signingConfigs {
