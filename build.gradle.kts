ec2a67ef29d67c69e2235ac480f235a5cfaf2b84
poorangryman <k-tarasov73@yandex.ru>
2026-09-01 17:29:34 +0400

    build: add Kotlin kapt for Room

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("com.android.application") version "9.3.2" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.kapt") version "2.4.10" apply false
 }
